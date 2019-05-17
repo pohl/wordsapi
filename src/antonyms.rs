d4c8c3f4f7a2cb642f66719c46f119c3cd3fb03a
Pohl Longsine <pohl.longsine@gmail.com>
2019-05-16 22:17:12 -0500

    add type for antonyms request

---
+++ src/antonyms.rs
@@ -0,0 +1,15 @@
+use crate::HasRequestType;
+use crate::RequestType;
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Antonyms {
+    pub word: String,
+    pub antonyms: Option<Vec<String>>,
+}
+
+impl HasRequestType for Antonyms {
+    fn request_type() -> RequestType {
+        RequestType::Antonyms
+    }
+}
